be3006478932c938762df33970a8a5d548fe043b
SneakyAssasin <112881964+iykwimhhe@users.noreply.github.com>
2026-08-30 18:30:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -457,21 +457,21 @@ if not stoch_signals:
 else:
 
     for i, stock in enumerate(stoch_signals, start=1):
-
-        name = str(stock["name"])[:6]
-
+    
+        name = str(stock["name"])[:5]
+    
         traded = stock["traded"] / 1_000_000_000
-
+    
         message += (
             f"{i:>2}. "
-            f"{name:<6} "
-            f"Rp{stock['close']:>7,.0f} "
-            f"{stock['change']:>7.2f}% "
-            f"{traded:>5.1f}B "
-            f"K{stock['k']:>4.1f} "
-            f"D{stock['d']:>4.1f}\n"
+            f"{name:<5} "
+            f"Rp{stock['close']:<6,.0f} "
+            f"{stock['change']:+.2f}% "
+            f"{traded:.1f}B "
+            f"K{stock['k']:.1f} "
+            f"D{stock['d']:.1f}\n"
         )
-
+    
 # ====================================
 # PRINT MESSAGE
 # ====================================
